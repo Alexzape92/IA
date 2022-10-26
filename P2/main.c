700c954addcc407e850a8825fd5187e274ff2908
Alejandro <alexguitarte@gmail.com>
2022-10-26 14:30:03 +0200

    Practica 2 terminada y funcional

---
+++ P2/main.c
@@ -0,0 +1,17 @@
+/*******************************************/
+/*             MAIN.C                      */
+/*                                         */
+/* Asignatura: Inteligencia Artificial     */
+/* Grado en Ingenieria Informatica - UCA   */
+/*******************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "busqueda.h"
+
+int main(){
+    solucionFin(busqueda());
+    return 0;
+}
